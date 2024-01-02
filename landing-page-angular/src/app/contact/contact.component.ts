2600c4bb0ddb883d9a62c58e48d4d48e30005848
erickubuntu <gestionecesaria@gmail.com>
2024-01-02 10:01:57 -0400

    2/01/2024

--- landing-page-angular/src/app/contact/contact.component.ts
+++ landing-page-angular/src/app/contact/contact.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
-export class ContactComponent {}
+export class ContactComponent {
+  contactForm: FormGroup;
+
+  constructor() {
+    this.contactForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+      this.contactForm.reset();
+    } else {
+      console.log('Ingrese los campos requeridos');
+      this.contactForm.markAllAsTouched;
+    }
+  }
+}
