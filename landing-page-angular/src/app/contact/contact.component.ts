6a49cc250943f39f16c62f3a4a89da9dd4a687f2
erickubuntu <gestionecesaria@gmail.com>
2024-01-02 14:26:58 -0400

    seccion form terminada

--- landing-page-angular/src/app/contact/contact.component.ts
+++ landing-page-angular/src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { NgClass, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -9,27 +10,43 @@ import {
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf, NgClass],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
   contactForm: FormGroup;
+  typeCI: string = 'Type of CI';
 
   constructor() {
     this.contactForm = new FormGroup({
-      name: new FormControl('', Validators.required),
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      lastName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      CI: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      typeCI: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
     });
   }
 
+  ngOnInit(): void {
+    // this.contactForm.valueChanges.subscribe((v) => console.log(v));
+    this.contactForm
+      .get('typeCI')
+      ?.valueChanges.subscribe((v) => (this.typeCI = v));
+  }
+
+  hasErrors(control1Name: string, errorType: string) {
+    return (
+      this.contactForm.get(control1Name)?.hasError(errorType) &&
+      this.contactForm.get(control1Name)?.touched
+    );
+  }
+
   onSubmit() {
-    if (this.contactForm.valid) {
-      console.log(this.contactForm.value);
-      this.contactForm.reset();
-    } else {
-      console.log('Ingrese los campos requeridos');
-      this.contactForm.markAllAsTouched;
-    }
+    console.log(this.contactForm);
+    // this.contactForm.reset();
   }
 }
