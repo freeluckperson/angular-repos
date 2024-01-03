e3defc47fefe160abf6415aab9827727e6900977
erickubuntu <gestionecesaria@gmail.com>
2024-01-02 21:12:26 -0400

    rollback

---
+++ landing-page-angular/src/app/contact/ci/ci.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ci',
+  standalone: true,
+  imports: [],
+  templateUrl: './ci.component.html',
+  styleUrl: './ci.component.css'
+})
+export class CiComponent {
+
+}
